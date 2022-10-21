ada1dfeb3fd7fc0818c31e3e87da20fae19c92dd
Rico Steinke <rsteinke@stud.hs-heilbronn.de>
2022-10-21 09:47:52 +0200

    add basic game results side channel

---
+++ modules/sidechannel/airhockey_sidechannel.py
@@ -0,0 +1,20 @@
+from mlagents_envs.environment import UnityEnvironment
+from mlagents_envs.side_channel.side_channel import SideChannel, IncomingMessage, OutgoingMessage
+
+import numpy as np
+import uuid
+
+
+class GameResultsSideChannel(SideChannel):
+    """
+    Side-Channel to communicate game results to determine Elo ratings with Airhockey environments.
+    """
+    def __init__(self) -> None:
+        super().__init__(uuid.UUID("2f487771-440f-4ffc-afd9-486650eb5b7b"))   
+
+    # implement this method to receive messages from Unity
+    def on_message_received(self, msg: IncomingMessage) -> None:
+        self.game_results = msg.read_float32_list()
+
+    
+
