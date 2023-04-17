6b8d3dec30f304ce842a352a29bd5decb66f9916
Rico Steinke <rico227@gmx.de>
2023-04-17 20:17:53 +0200

    add training history plot from tensorboard exports

--- plot.py
+++ plot.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import argparse
 import numpy as np
 import datetime as dt
+import glob
+import csv
+import seaborn as sns
+import datetime
 
 def main():
     parser = argparse.ArgumentParser()
@@ -11,21 +15,23 @@ def main():
     parser.add_argument('-p', '--path', help="Specify the path to the data file.", type=str, required=True)
     parser.add_argument('-s', '--save', help="Save the plot to the specified path.", type=str, required=False)
     parser.add_argument('-sh', '--show', help="Show the plot.", type=bool, required=False)
+    parser.add_argument('--dpi', help="Specify the DPI of the plot. Hence the resolution if the plot should be saved.", type=int, required=False, default=400)
+    parser.add_argument('-t', '--title', help="Specify the title of the plot.", type=str, required=False, default=None)
     args = parser.parse_args()
     
     if args.mode == 'rating':
-        plot = plot_rating(args.path)
+        plot = plot_rating(args.path, args.title)
     elif args.mode == 'training':
-        plot = plot_training()
+        plot = plot_training(args.path, args.title)
 
     if args.save is not None:
         now = dt.datetime.now()
         formatted_datetime = now.strftime('%Y%m%d_%H%M%S')
-        plot.savefig(f"{args.save}/{formatted_datetime}_{args.mode}_plot.png", dpi=400)
+        plot.savefig(f"{args.save}/{formatted_datetime}_{args.mode}_plot.png", dpi=args.dpi)
     if args.show is True:        
         plot.show(block=True)
 
-def plot_rating(path):
+def plot_rating(path, title):
     # Load the CSV file using pandas
     df = pd.read_csv(path)
 
@@ -78,15 +84,61 @@ def plot_rating(path):
     # Add labels and legend to the plot
     plt.xlabel('Rating Period')
     plt.ylabel('Rating and Rating Deviation (Errorbars)')
-    plt.title('Player Ratings Over Time')
+    if title is not None:
+        plt.title(title)
+    else:
+        plt.title('Player Ratings Over Time')
     # plot the legend below the plot   
     plt.legend(loc="upper center", bbox_to_anchor=(0.5, -0.15), ncol=1)
     plt.subplots_adjust(bottom=0.25)    
     return plt
 
-def plot_training():
-    return True
+def plot_training(path, title):
+    # Add wildcard support for path
+    path = path + '/*.csv'
+    columns_to_check = ['Value', 'Wall time', 'Step']
+    tensorboard_files = []
+    for file in glob.glob(path):
+        if check_columns(file, columns_to_check):
+            tensorboard_files.append(file)
+            print(f"Plotting data from {file} because it does contain the columns to qualify as a tensorboard export.")
+        else:
+            print(f"Skipping {file} because it does not contain the columns to qualify as a tensorboard export.")
 
+    # read each csv file in directory and add a column for the agent number
+    data = pd.concat([pd.read_csv(f).assign(Agent=f"Agent {i}") for i, f in enumerate(tensorboard_files)], ignore_index = True)
+
+    data['Reward'] = data['Value']
+    data['Time [h]'] = data['Wall time']
+    data['Time [h]'] = data['Time [h]'].apply(datetime.datetime.fromtimestamp)
+    # Calculate the difference between the first and last datetime values
+    difference = data['Time [h]'].iloc[-1] - data['Time [h]'].iloc[0]
+    # Calculate the difference in hours
+    hours = difference.total_seconds() / 3600
+    # calculate time past since training started
+    data['Time [h]'] = data['Time [h]'].apply(lambda x: (x - data['Time [h]'].iloc[0]).total_seconds() / 3600)
+
+    data_wide = data.pivot(index='Time [h]', columns='Agent', values='Reward')
+    for i in range(8):
+        data_wide[f'Agent {i}'] = data_wide[f'Agent {i}'].ewm(alpha=(1-0.999)).mean()
+
+    print(f"Total training time in hours: {hours}")
+
+    # Plot the data
+    sns.set_style("darkgrid")
+    if title is not None:
+        sns.lineplot(data=data_wide).set_title(title)
+    else:
+        sns.lineplot(data=data_wide).set_title('Durchschnittliche Reward-Entwicklung')    
+    plt.ylabel('Reward')
+    return plt
+
+def check_columns(file_path, columns):
+    with open(file_path, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        header = reader.fieldnames
+        return all(column in header for column in columns)
+    
 if __name__ == '__main__':
     main()
 
