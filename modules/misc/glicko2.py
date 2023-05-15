9941e93649663f5ef7bb6113fe3eaa8c4e49d1d8
Rico Steinke <rico227@gmx.de>
2023-05-15 09:40:05 +0200

    improve glicko calculation

--- modules/misc/glicko2.py
+++ modules/misc/glicko2.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 # calculate standard Glicko2
-def calculate_updated_glicko2(rating, rating_deviation, volatility, opponents_in_period, tau=0.2, rd_threshold=30):
+def calculate_updated_glicko2(rating, rating_deviation, volatility, opponents_in_period, tau=0.5, rd_threshold=30):
     """
     Function to calculate an updated glicko rating for a player a given the score of a played match between player a and
     player b as well as their previous rating estimates.
@@ -19,7 +19,6 @@ def calculate_updated_glicko2(rating, rating_deviation, volatility, opponents_in
     # set prior to application of the system.
     # (a) If the player is unrated, set the rating to 1500 and the RD to 350. Set the volatility to 0.06.
     # (b) If the player is rated, use the rating, rating deviation and volatility from the previous rating period.
-    # These values are stored in the corresponding csv files and should be used to initialize this function.
 
     # create to store the opponents glicko2 calculations
     opponents = pd.DataFrame(columns=['my', 'phi', 'g', 'E', 's'])
@@ -45,7 +44,7 @@ def calculate_updated_glicko2(rating, rating_deviation, volatility, opponents_in
     volatility_updated = _calculate_volatility(volatility, delta, v, tau, converted_rating_deviation)
 
     # Step 6: Compute the new pre rating period deviation phi*.
-    rating_deviation_pre_rating_period = math.sqrt(converted_rating_deviation ** 2 + volatility_updated ** 2)
+    rating_deviation_pre_rating_period = math.sqrt((converted_rating_deviation ** 2) + (volatility_updated ** 2))
 
     # Step 7: Update the rating and rating deviation to the new values
     rating_updated, rating_deviation_updated = _update_rating(converted_rating, rating_deviation_pre_rating_period, v, opponents['g'].to_list(), opponents['E'].to_list(), opponents['s'].to_list())
@@ -109,37 +108,48 @@ def _calculate_f(x, delta, v, tau, rating_deviation, a):
 
 
 # calculate volatility
-def _calculate_volatility(volatility, delta, v, tau, rating_deviation, epsilon=0.000001):
+def _calculate_volatility(volatility, delta, v, tau, rating_deviation, epsilon=0.000001, max_iterations=1000):
+    # Step 5.2: Set the initial values of the iterative algorithm
     a = np.log(volatility ** 2)
-    if delta ** 2 > (rating_deviation ** 2) + v:
-        b = np.log(delta ** 2 - (rating_deviation ** 2) - v)
-    else:
+    A = a
+    if (delta ** 2) > ((rating_deviation ** 2) + v):
+        B = np.log(delta ** 2 - (rating_deviation ** 2) - v)
+    if (delta ** 2) <= ((rating_deviation ** 2) + v):
         k = 1
         while _calculate_f(a - k * tau, delta, v, tau, rating_deviation, a) < 0:
-            k += 1            
-        b = a - k * tau
-    f_a = _calculate_f(a, delta, v, tau, rating_deviation, a)
-    f_b = _calculate_f(b, delta, v, tau, rating_deviation, a)
-    while abs(b - a) > epsilon:
-        if abs(b - a) <= epsilon:
+            k += 1
+            B = a - (k * tau)
+        B = a - (k * tau)
+    # Step 5.3: Calculate f(a) and f(b)
+    f_a = _calculate_f(A, delta, v, tau, rating_deviation, a)
+    f_b = _calculate_f(B, delta, v, tau, rating_deviation, a)
+    # Step 5.4: While |b-a| > e, carry out the following steps.
+    iterations = 0
+    while abs(B - A) > epsilon:
+        iterations += 1
+        if iterations > max_iterations:
+            print('Max iterations reached')
             break
+        # if abs(B - A) <= epsilon:
+        #     break
         else:
-            c = a + ((a - b) * f_a) / (f_b - f_a)
-            f_c = _calculate_f(c, delta, v, tau, rating_deviation, a)
+            C = A + ((A - B) * f_a) / (f_b - f_a)
+            f_c = _calculate_f(C, delta, v, tau, rating_deviation, a)
             if f_c * f_b <= 0:
-                a = b
+                A = B
                 f_a = f_b
             else:
                 f_a = f_a / 2
-            b = c
-            f_b = f_c        
-    volatility_updated = math.exp(a / 2)
+            B = C
+            f_b = f_c
+    # Step 5.5: Update the volatility        
+    volatility_updated = math.exp(A / 2)
     return volatility_updated
 
 
 def _update_rating(rating, rating_deviation_pre_rating_period, v, g_list, e_list, score_list):
     rating_deviation_updated = 1 / math.sqrt((1 / (rating_deviation_pre_rating_period ** 2)) + (1 / v))
-    rating_updated = rating + rating_deviation_updated ** 2 * sum([g * (score - e) for g, score, e in zip(g_list, score_list, e_list)])
+    rating_updated = rating + (rating_deviation_updated ** 2) * sum([g * (score - e) for g, score, e in zip(g_list, score_list, e_list)])
     return rating_updated, rating_deviation_updated
 
 
